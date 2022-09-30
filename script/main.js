72ccf3cee96adff442a549227f8380e3db56b936
Destinee Fa <destinee.fa@gmail.com>
2022-09-30 10:27:43 +0800

    added audio button for play and pause

--- script/main.js
+++ script/main.js
@@ -7,21 +7,25 @@ const $instructBtn = $("#instructions-btn")
 const $homeBtn = $(".home-btn")
 const $moreBtn = $(".more-btn")
 const $backBtn = $(".back-btn")
-const screenAudio = new Audio("audio/screens.mp3")
+const $screenAudio = $("#screen-audio")
+const $audioBtn = $("#audio-button")
 
- // Play Background Music
-const playScreenAudio = () => {
-  if (screenAudio.paused) {
-    screenAudio.play()
-    $(document).off('click', playScreenAudio)
-  } else {
-    screenAudio.currentTime = 0
-  }
+// LoopAudio
+const loopAudio = () => {
+  $screenAudio[0].currentTime = 0
+  $screenAudio[0].play()
 }
-$(document).on('click', playScreenAudio)
+
+ // Play Background Music on Loop
+const playScreenAudio = () => {
+  $screenAudio[0].paused ? $screenAudio[0].play() : $screenAudio[0].pause()
+}
 
 const game = new Game()
 
+// Event Listeners
+$audioBtn.on('click', playScreenAudio)
+$screenAudio.on("ended", loopAudio)
 $startBtn.on("click", game.startGame)
 $retryBtn.on("click", game.startGame)
 $instructBtn.on("click", game.showFirstInstructions)
